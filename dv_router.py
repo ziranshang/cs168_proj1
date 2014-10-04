5821419cd5ec7685b58489afca9789bf47445b0e
Ziran Shang <zshang2@berkeley.edu>
2014-10-04 14:58:55 -0700

    initial commit

---
+++ dv_router.py
@@ -0,0 +1,14 @@
+from sim.api import *
+from sim.basics import *
+
+'''
+Create your distance vector router in this file.
+'''
+class DVRouter (Entity):
+    def __init__(self):
+        # Add your code here!
+        pass
+
+    def handle_rx (self, packet, port):
+        # Add your code here!
+        raise NotImplementedError
